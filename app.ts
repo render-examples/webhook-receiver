35b03ca141c3db7b97709dc63753b7cc0934e6de
Max Eshleman <max@render.com>
2025-02-13 12:54:49 -0700

    validate webhook

--- app.ts
+++ app.ts
@@ -1,10 +1,35 @@
 import express from "express";
+import {Request, Response} from "express";
+import { Webhook, WebhookUnbrandedRequiredHeaders, WebhookVerificationError } from "standardwebhooks"
+
 const app = express();
 const port = process.env.PORT || 3001;
+const renderWebhookSecret = process.env.RENDER_WEBHOOK_SECRET || '';
 
-app.post("/webhook", express.raw({type: 'application/json'}), (req, res) => {
-    console.log({body: JSON.parse(req.body)});
-    res.send({})
+app.post("/webhook", express.raw({type: 'application/json'}), (req: Request, res: Response, next: any) => {
+    const headers: WebhookUnbrandedRequiredHeaders = {
+        "webhook-id": req.header("webhook-id") || "",
+        "webhook-timestamp": req.header("webhook-timestamp") || "",
+        "webhook-signature": req.header("webhook-signature") || ""
+    }
+
+    try {
+        const wh = new Webhook(renderWebhookSecret);
+        wh.verify(req.body, headers);
+    } catch (error) {
+       return next(error)
+    }
+
+    res.status(200).send({})
+});
+
+app.use((err: any, req: Request, res: Response, next: any) => {
+    console.error(err);
+    if (err instanceof WebhookVerificationError) {
+        res.status(400).send({})
+    } else {
+        res.status(500).send({})
+    }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
